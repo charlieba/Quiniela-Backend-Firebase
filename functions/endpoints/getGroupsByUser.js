fd1d99434fd6b43b387584761e96399b006ef71e
giovanib <carlos.barillas@fox.com>
2018-05-03 18:28:46 -0600

    getGroupsByUser

--- functions/endpoints/getGroupsByUser.js
+++ functions/endpoints/getGroupsByUser.js
@@ -1,15 +1,11 @@
 module.exports={
     getGroupsByUser: function(req,res,admin,email,tournamentId){
-        var ref_db = admin.database().ref('/groups');
+        var ref_db = admin.database().ref('/groupsByMembers');
         res.set('Content-Type', 'application/json');
             try {
-                userExists(email,ref_db,(response)=>{
-                  var key="";
-                  if(response===0){
-                    res.end("{\"errror\":\"User already exits\"}");
-                  }else{
-                    res.end("{\"errror\":\"User already exits\"}");
-                  }
+              getGroupsByUser(email,ref_db,(response)=>{
+                    response=JSON.stringify(response);
+                    res.end("{\"groups\":"+response.toString()+"}");
                 });
               } catch (error) {
                  res.end("{\"error\":\""+error+"\"}");
@@ -18,14 +14,15 @@ module.exports={
     }
 }
 
-function userExists(email,ref_db,callback){
-    var counter=0;
-    console.log(email);
-    ref_db.child("members").orderByChild("email").equalTo("giovanib07@gmail.com").on("value", (snapshot)=>{
-      console.log(snapshot.val());
-      counter=snapshot.numChildren(); 
-      console.log(snapshot.numChildren());
-      callback(counter);
+function getGroupsByUser(email, ref_db,callback){
+  var groups=[];
+  ref_db.orderByChild("email").equalTo(email).once("value", (snapshot)=>{
+      snapshot.forEach((childSnapshot)=>{
+        groups.push(childSnapshot.val().groupName);
     });
-  }
-  
+    callback(groups);
+  },(errorObject)=>{
+      console.log("The read failed: " + errorObject.code);
+      callback(0);
+    });
+}
